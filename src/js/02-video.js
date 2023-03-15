f39fde09455a7e5d19a55ef5fd37598a898c8348
Yana Lapach <y.lapach@gmail.com>
2023-03-15 21:44:41 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VPCurrentTime = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem(VPCurrentTime, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VPCurrentTime)) || 0);
