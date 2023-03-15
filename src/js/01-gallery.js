a2909b20e3e856470adf8717b2de8f5a48a1b441
Yana Lapach <y.lapach@gmail.com>
2023-03-15 21:19:40 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const gallerySet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', gallerySet);
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
